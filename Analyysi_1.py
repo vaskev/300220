f3abdf61a2c9ebdbf99656a380e1f4cdf8ae318d
vaskev <vesa.vaskelainen@eksal.fi>
2019-04-26 23:51:29 +0300

    Testi 2

--- Analyysi_1.py
+++ Analyysi_1.py
@@ -15,16 +15,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-dataRaw = pd.read_csv('/home/vaske/to_backup/300220/Datoja/data.txt', delimiter=';')
-dataRaw_arr = dataRaw.iloc[0:557,0:2].values
+data_raw_read = pd.read_csv('/home/vaske/to_backup/300220/Datoja/data.txt', delimiter=';')
+data_raw =data_raw_read.iloc[0:557,0:2]
+max_value_index= data_raw.idxmax(0)
 
+
+# debug tiedosto
+dataRaw_arr = data_raw.values
 dataRaw_pandas=pd.DataFrame(dataRaw_arr)
 dataRaw_pandas.to_csv('/home/vaske/to_backup/300220/Datoja/data_from_calc.txt', sep=';')
 
 freq =dataRaw_arr[:,0]
 values=dataRaw_arr[:,1]
 
-plt.plot(freq,values)
+pointer_f = freq[max_value_index[1]]
+pointer_v = values[max_value_index[1]]
+
+freq_r  = round ( freq  [max_value_index[1]] , 2)
+value_r = round ( values[max_value_index[1]] , 2)
+
+print('f =     '+str(freq_r))
+print('Value = '+str(value_r))
+
+raami = plt.figure()
+kuvaaja = raami.add_subplot(1,1,1)
+kuvaaja.plot(freq,values)
+teksti = str(freq_r)+"MHz,"+str(value_r)
+kuvaaja.annotate(teksti,
+                 xy=(pointer_f,pointer_v  ),
+                 xytext = (pointer_f +5 ,pointer_v +5),
+                 arrowprops=dict(arrowstyle="->",connectionstyle="arc,angleA=45,armA=10,rad=10")
+                 )
+
 plt.show()
 
 print('DONE')
